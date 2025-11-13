e61e39ac8a62248dcc0a7cf492f629beb0365059
Ignacio Cebral De Juan - Roncero <es00697029@prosegur.com>
2025-11-13 17:16:22 +0100

    fix: Correct metric inflation and config.js

--- definitions/custom/config.js
+++ definitions/custom/config.js
@@ -1,8 +1,7 @@
-
 const { coreConfig } = require("../../includes/core/default_config");
 
 const customConfig = {
-  ...
+  ...coreConfig,
   CLICK_IDS_ARRAY: [
     ...coreConfig.CLICK_IDS_ARRAY,
     { name: "gclid", sources: ["url"], type: "string" },
