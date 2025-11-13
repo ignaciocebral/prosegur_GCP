3361df875db94e207099031b5a581c4180ab6e70
ignaciocebral <ignacio.cebral@hotmail.com>
2025-11-13 18:20:59 +0100

    Fix Google Ads click enrichment

--- definitions/custom/config.js
+++ definitions/custom/config.js
@@ -2,10 +2,6 @@ const { coreConfig } = require("../../includes/core/default_config");
 
 const customConfig = {
   ...coreConfig,
-  CLICK_IDS_ARRAY: [
-    ...coreConfig.CLICK_IDS_ARRAY,
-    { name: "gclid", sources: ["url"], type: "string" },
-  ],
 };
 
 module.exports = { customConfig };
